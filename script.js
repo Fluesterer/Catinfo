7a1185de60ec77c12e7cfd2742d106da9af063ba
Fluesterer <nico.friesen1bausa@gmail.com>
2025-02-04 14:49:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Dunkelmodus umschalten
 document.getElementById('theme-toggle').addEventListener('click', function() {
   const body = document.body;
   body.classList.toggle('dark-theme');
@@ -10,16 +11,19 @@ document.getElementById('theme-toggle').addEventListener('click', function() {
   }
 });
 
-
-// Öffnen des Modals
+// Modal öffnen
 function openModal(title, description, imgSrc) {
+  // Modal-Daten setzen
   document.getElementById('modal-title').innerText = title;
   document.getElementById('modal-description').innerText = description;
   document.getElementById('modal-image').src = imgSrc;
   document.getElementById('modal').style.display = 'block';
 }
 
-// Schließen des Modals
+// Modal schließen
 function closeModal() {
   document.getElementById('modal').style.display = 'none';
 }
+
+// Event Listener für das Schließen des Modals
+document.getElementById('modal-close').addEventListener('click', closeModal);
